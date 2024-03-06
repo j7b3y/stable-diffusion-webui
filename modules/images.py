20a604fbfd1724b4ee089e4cfdc08b41aadc13fc
j7b3y <glowing3386@proton.me>
2024-03-06 11:51:09 +0900

    Store items containing the specified prompt in the specified folder
    spsf<prompt1,ptompt2|default|folder>

--- modules/images.py
+++ modules/images.py
@@ -331,6 +331,7 @@ class FilenameGenerator:
 
         'sampler': lambda self: self.p and self.p.sampler_name,
         'seed': lambda self: self.seed and str(self.seed) or '',
+        'spsf': lambda self, *args: self.spsf(*args),
         'steps': lambda self: self.p and self.p.steps,
         'styles': lambda self: self.p and ", ".join([style for style in self.p.styles if not style == "None"]) or "None",
         'uuid': lambda self: str(uuid.uuid4()),
@@ -370,6 +371,24 @@ class FilenameGenerator:
                 default = division[1] if len(division) > 1 else ""
                 if lower.find(expected) >= 0:
                     outres = f'{outres}{expected}'
+                else:
+                    outres = outres if default == "" else f'{outres}{default}'
+        return outres
+    
+    def spsf(self, *args):
+        lower = self.prompt.lower()
+        if self.p is None or self.prompt is None:
+            return None
+        outres = ""
+        for arg in args:
+            if arg != "":
+                division = arg.split("|")
+                expected = division[0].lower()
+                promlist = expected.split(",")
+                default = division[1] if len(division) > 1 else ""
+                recipient = division[2]
+                if any(item in lower for item in promlist):
+                    outres = f'{outres}{recipient}'
                 else:
                     outres = outres if default == "" else f'{outres}{default}'
         return outres
