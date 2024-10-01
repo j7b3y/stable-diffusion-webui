2a1ba6d44a36e4094c117f1114f21c23d224ebca
j7b3y <glowing3386@proton.me>
2024-10-01 17:51:54 +0900

    remove splash

--- javascript/loader.js
+++ javascript/loader.js
@@ -23,34 +23,3 @@ async function preloadImages() {
     console.error('Error preloading images:', error);
   }
 }
-
-async function createSplash() {
-  const dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-  log('createSplash', { theme: dark ? 'dark' : 'light' });
-  const num = Math.floor(9.99 * Math.random());
-  const splash = `
-    <div id="splash" class="splash" style="background: ${dark ? 'black' : 'white'}">
-      <div class="loading"><div class="loader"></div></div>
-      <div id="motd" class="motd""></div>
-    </div>`;
-  document.body.insertAdjacentHTML('beforeend', splash);
-  await preloadImages();
-  const imgEl = `<div id="spash-img" class="splash-img" alt="logo" style="background-image: url(file=html/logo-bg-${dark ? 'dark' : 'light'}.jpg), url(file=html/logo-bg-${num}.jpg); background-blend-mode: ${dark ? 'multiply' : 'lighten'}"></div>`;
-  document.getElementById('splash').insertAdjacentHTML('afterbegin', imgEl);
-  fetch('/sdapi/v1/motd')
-    .then((res) => res.text())
-    .then((text) => {
-      const motdEl = document.getElementById('motd');
-      if (motdEl) motdEl.innerHTML = text.replace(/["]+/g, '');
-    })
-    .catch((err) => console.error('getMOTD:', err));
-}
-
-async function removeSplash() {
-  const splash = document.getElementById('splash');
-  if (splash) splash.remove();
-  log('removeSplash');
-  log('startupTime', Math.round(performance.now() - appStartTime) / 1000);
-}
-
-window.onload = createSplash;
